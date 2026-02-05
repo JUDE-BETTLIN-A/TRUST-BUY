9e1527007f39d87ffc2c607c29733b26dabf2a13
JudeBettlin A <judebettlin@gmail.com>
2026-02-05 22:56:08 +0530

    Fix Google OAuth provider configuration

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,14 +1,21 @@
 import NextAuth from "next-auth";
 import CredentialsProvider from "next-auth/providers/credentials";
-import GoogleProvider from "next-auth/providers/google";
+import Google from "next-auth/providers/google";
 import { authConfig } from "./auth.config";
 
 const nextAuth = NextAuth({
   ...authConfig,
   providers: [
-    GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID || "",
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET || "",
+    Google({
+      clientId: process.env.GOOGLE_CLIENT_ID!,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+      authorization: {
+        params: {
+          prompt: "consent",
+          access_type: "offline",
+          response_type: "code"
+        }
+      }
     }),
     CredentialsProvider({
       name: "Credentials",
